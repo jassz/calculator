eea88e8b48bd1b8ce73dc50a21d8c04eee677a65
najanadhirah <najanadhirah11@gmail.com>
2023-03-16 17:15:08 +0800

    init commit for functional calculator

---
+++ src/components/Demo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Demo.module.css'
+
+function Demo() {
+  return (
+    <div className={classes.inner}>
+      
+    </div>
+  )
+}
+
+export default Demo
---
+++ src/components/a.js
@@ -0,0 +1,4 @@
+const a = "2+2";
+const b = Function("return" +a)
+
+console.log(b);
